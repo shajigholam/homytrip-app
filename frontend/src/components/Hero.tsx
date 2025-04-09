ad1b3f22ac90c7cbf2d797d545da15b3f6780114
shajigholam <shajigholam@myseneca.ca>
2025-04-09 17:10:04 -0400

    added footer

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -2,7 +2,7 @@ const Hero = () => {
     return (
         <div className="bg-indigo-800 pb-16">
             <div className="container mx-auto flex flex-col gap-2">
-                <h1 className="text-5xl text-white font-bold">Find your next adventure with us!</h1>
+                <h1 className="text-5xl text-white font-bold">Your next adventure is here!</h1>
                 <p className="text-2xl text-white">Search low prices on hotels for your dream vacation...</p>
             </div>
         </div>
