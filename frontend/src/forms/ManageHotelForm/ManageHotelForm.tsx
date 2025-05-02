4fdf14dc85d59ce418778c4bef1b6a248232aa0c
shajigholam <shajigholam@myseneca.ca>
2025-05-02 11:24:19 -0400

    adding handle submit

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -22,14 +22,46 @@ export type HotelFormData = {
 
 const ManageHotelForm = () => {
   const formMethods = useForm<HotelFormData>();
+  const {handleSubmit} = formMethods;
+
+  const onSubmit = handleSubmit((formDataJson: HotelFormData) => {
+    //create a new formdata obj and call our API
+    const formData = new FormData();
+    formData.append("name", formDataJson.name);
+    formData.append("city", formDataJson.city);
+    formData.append("country", formDataJson.country);
+    formData.append("description", formDataJson.description);
+    formData.append("type", formDataJson.type);
+    formData.append("pricePerNight", formDataJson.pricePerNight.toString());
+    formData.append("starRating", formDataJson.starRating.toString());
+    formData.append("adultCount", formDataJson.adultCount.toString());
+    formData.append("childCount", formDataJson.childCount.toString());
+
+    formDataJson.facilities.forEach((facility, index) => {
+      formData.append(`facilities[${index}]`, facility);
+    });
+
+    // in the backend Multer will collect all files with the same field name (imageFiles) into an array.( upload.array("imageFiles") )
+    Array.from(formDataJson.imageFiles).forEach(imageFile => {
+      formData.append("imageFiles", imageFile);
+    });
+  });
   return (
     <FormProvider {...formMethods}>
-      <form className="flex flex-col gap-10">
+      <form className="flex flex-col gap-10" onSubmit={onSubmit}>
         <DetailsSection />
         <TypeSection />
         <FacilitiesSection />
         <GuestsSection />
         <ImagesSection />
+        <span className="flex justify-end">
+          <button
+            type="submit"
+            className="bg-indigo-600 text-white p-2 font-bold hover:bg-indigo-500 text-xl"
+          >
+            Save
+          </button>
+        </span>
       </form>
     </FormProvider>
   );
