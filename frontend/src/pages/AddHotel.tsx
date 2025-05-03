c2eb61a05cd4dcd5ff5501c8aa5cb1d3348433dc
shajigholam <shajigholam@myseneca.ca>
2025-05-02 20:23:45 -0400

    fixed the issue

--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -3,22 +3,22 @@ import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
 import {useAppContext} from "../contexts/AppContext";
 import * as apiClient from "../api-client";
 
-const {showToast} = useAppContext();
-
-const mutation = useMutation({
-  mutationFn: apiClient.addMyHotel,
-  onSuccess: () => {
-    showToast({message: "Hotel Saved", type: "SUCCESS"});
-  },
-  onError: () => {
-    showToast({message: "Error saving hotel", type: "ERROR"});
-  },
-});
-
-const handleSave = (hotelFormData: FormData) => {
-  mutation.mutate(hotelFormData);
-};
 const AddHotel = () => {
+  const {showToast} = useAppContext();
+
+  const mutation = useMutation({
+    mutationFn: apiClient.addMyHotel,
+    onSuccess: () => {
+      showToast({message: "Hotel Saved", type: "SUCCESS"});
+    },
+    onError: () => {
+      showToast({message: "Error saving hotel", type: "ERROR"});
+    },
+  });
+
+  const handleSave = (hotelFormData: FormData) => {
+    mutation.mutate(hotelFormData);
+  };
   return <ManageHotelForm onSave={handleSave} isLoading={mutation.isPending} />;
 };
 
