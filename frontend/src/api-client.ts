1d61b292a7bb64164a0185b8bf01923a132c61bd
shajigholam <shajigholam@myseneca.ca>
2025-05-05 20:37:25 -0400

    added api call for fetching myhotels

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,5 +1,6 @@
 import {RegisterFormData} from "./pages/Register";
 import {SignInFormData} from "./pages/SignIn";
+import {HotelType} from "../../backend/src/models/hotel";
 
 // add empty string coz in production we don't have a seperate URL for frontend
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
@@ -70,4 +71,16 @@ export const addMyHotel = async (hotelFormData: FormData) => {
     throw new Error("Failed to add hotel");
   }
   return response.json(); //check the network tab
+};
+
+export const fetchMyHotels = async (): Promise<HotelType[]> => {
+  const response = await fetch(`${API_BASE_URL}/api/my-hotels`, {
+    credentials: "include",
+  });
+
+  if (!response.ok) {
+    throw new Error("Error fetching hotels");
+  }
+
+  return response.json();
 };
